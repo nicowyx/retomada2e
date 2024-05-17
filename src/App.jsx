0ad4314db503d99d3fb4ac7acfe18b98b27608e5
nicowyx <nico.wyx@gmail.com>
2024-05-17 08:31:07 -0300

    card1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react"
+import './App.css'
 
+const Card1 () => (
+<section>
+  <header>
+     AULA INICIAL DE REACTJS
+  </header>
+  <content>
+    Vamos codar em ReactJS
+  </content>
+  <footer>
+    @itb2024
+  </footer>
+</section>
+)
 const App = () => (
-  <>
-    AULA INICIAL DE REACTJS
-  </>
+  <div className='App'>
+    
+
+
+  </div>
 )
 
 export default App 
